1fd63067c90a4a777ce2f720d48677853dfb47e3
IssamLynx <hm_bakha@esi.dz>
2023-03-14 17:37:22 +0100

    Passed props to button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
-
+  @Input() color = ''; // decorate the property with @Input()
+  @Input() text = ''; // decorate the property with @Input()
 }
